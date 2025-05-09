b457024467243c1cc320fd926357b19ba5379aea
covey-codes <dustindark7@gmail.com>
2025-05-09 11:13:40 +0100

    minor update to deploy to vercel

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import { AppRoutes } from "./router";
 import { ThemeProvider } from "@/shared/context/ThemeProvider.tsx";
 
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
